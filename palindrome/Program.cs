ed93e895ae65e8d72decc9fa98601c30f0708aa1
SeshagiriSriram <SeshagiriSriram@gmail.com>
2025-10-06 16:47:07 +0530

    test

--- palindrome/Program.cs
+++ palindrome/Program.cs
@@ -1,5 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 bool isPalindrome = false;
-string a = "madam"; // விகடகவி
+string a = "sad";
 
+
+for (int i = 0; i < a.Length / 2; i++)
+{
+    if (a[i] != a[a.Length - i - 1])
+    {
+        isPalindrome = false;
+        break;
+    }
+    isPalindrome = true;
+}
+
+Console.WriteLine($"The given string {a} is palindrome: {isPalindrome}");
