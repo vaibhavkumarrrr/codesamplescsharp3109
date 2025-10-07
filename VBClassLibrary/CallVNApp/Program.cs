24991658ac77eff168d0eeb2cd0d1e1139c32c98
SeshagiriSriram <SeshagiriSriram@gmail.com>
2025-10-07 09:56:58 +0530

    addded Code for VB and ILCode

---
+++ VBClassLibrary/CallVNApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using VBClassLibrary;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(VBClass.Add(2, 4)); // Call the Add function from the VBClassLibrary
+        Console.WriteLine(VBClass.Add(3, 7)); // Call the Add function from the VBClassLibrary
+    }
+}
---
+++ VBClassLibrary/VBClassLibrary/Class1.vb
@@ -0,0 +1,8 @@
+Public Class VBClass
+
+    Public Shared Function Add(x As Integer, y As Integer) As Integer
+        Return x + y
+    End Function
+
+
+End Class
