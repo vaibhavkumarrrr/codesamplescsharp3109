ac2f9fb4669cc9641dcbaf9941dec803c6e43c18
SeshagiriSriram <SeshagiriSriram@gmail.com>
2025-10-09 10:00:58 +0530

    added new folders

---
+++ CoreCSharp/Apps/ReferenceDataTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace csharp.training.congruent
+{
+    public struct MutablePoint
+    {
+        public int X; public int Y; public MutablePoint(int x, int y) => (X, Y) = (x, y);
+        public override string ToString() => $"({X}, {Y})";
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p1 = new MutablePoint(1, 2);        // p1  = 1, 2
+            var p2 = p1;
+            p2.Y = 200;                                     // p2 = 1,200, p1 Is still 1,2 
+            Console.WriteLine($"{nameof(p1)} after {nameof(p2)} is modified: {p1}");
+            Console.WriteLine($"{nameof(p2)}: {p2}");
+            MutateAndDisplay(p2);
+            Console.WriteLine($"{nameof(p2)} after passing to a method: {p2}");
+        }
+
+        private static void MutateAndDisplay(MutablePoint p)
+        {
+            p.X = 100;
+            Console.WriteLine($"Point mutated in a method: {p}");
+        }
+
+    }
+}
