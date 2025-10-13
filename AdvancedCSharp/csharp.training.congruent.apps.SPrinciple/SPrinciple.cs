37fd4471854d53fd62ef48fb870170d26b3549cd
SeshagiriSriram <SeshagiriSriram@gmail.com>
2025-10-14 01:19:32 +0530

    Added Code for S of SOLID Principle

---
+++ AdvancedCSharp/csharp.training.congruent.apps.SPrinciple/SPrinciple.cs
@@ -0,0 +1,14 @@
+﻿using csharp.training.congruent.classes; 
+namespace csharp.training.congruent.apps.SPrinciple
+{
+    internal class SPrinciple
+    {   
+        static  void Main(string[] _)
+        {
+            Book a = new("Grapes of Wrath", "John Steinbeck", "A great Book", "9780143039433");
+            BookPrinter.PrintBook(a);
+            Console.WriteLine(a.IsBookWrittenBy("John Steinbeck"));
+            a.DescribeBook(); 
+        }
+    }
+}
---
+++ AdvancedCSharp/csharp.training.congruent.classes/BookPrinter.cs
@@ -0,0 +1,17 @@
+﻿namespace csharp.training.congruent.classes
+{
+    using System;
+    using System.Net.Http.Headers;
+    using System.Reflection.Metadata.Ecma335;
+    using System.Runtime.CompilerServices;
+
+    public class BookPrinter
+    {
+        public static void PrintBook(Book a)
+        {
+            Console.WriteLine("******** Book Info"); 
+            Console.WriteLine(a.ToString());
+        }
+    }
+}
+
