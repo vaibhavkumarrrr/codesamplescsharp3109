23a9932a324ece4490c1c7053a576336473e3bd8
SeshagiriSriram <SeshagiriSriram@gmail.com>
2025-10-24 13:18:18 +0530

    Added explanation on covariance

---
+++ CoreCSharp/Apps/DidYouUnderstand/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace csharp.training.congruent.apps
+{
+        public class A
+        {
+            public string X { get; set; } = "A";
+        }
+
+        public class B : A
+        {
+            public new string X { get; set; } = "B";
+        }
+        internal class Program
+        {
+            static void Main(string[] _)
+            {
+                A obj = new B();
+                string tmp = obj is B b ? b.X : obj.X;
+                Console.WriteLine($"Value of X: {tmp}");
+                obj = new A();
+                tmp = obj is B c ? c.X : obj.X;
+                Console.WriteLine($"Value of X: {tmp}");
+        }
+        }
+    }
